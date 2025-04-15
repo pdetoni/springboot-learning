d13463680a3e4a7ec3dc12e98a2290da0b990143
pdetoni <pdpereira2003@gmail.com>
2025-04-15 10:41:38 -0300

    testing h2 database, test profile and JPA

--- src/main/java/com/pdetoni/course/entities/User.java
+++ src/main/java/com/pdetoni/course/entities/User.java
@@ -1,12 +1,19 @@
 package com.pdetoni.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
