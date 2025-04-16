538fc56f35fd39ffff78b059f0751da647645a77
pdetoni <pdpereira2003@gmail.com>
2025-04-16 09:13:40 -0300

    implementing service layer and component registration

---
+++ src/main/java/com/pdetoni/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pdetoni.course.services;
+
+import com.pdetoni.course.entities.User;
+import com.pdetoni.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+
+}
