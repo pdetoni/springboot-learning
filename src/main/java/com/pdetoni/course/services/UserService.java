869b05cf2cd8dac87d30812b17af48053399cce6
pdetoni <pdpereira2003@gmail.com>
2025-04-23 08:50:08 -0300

    exception handling findById

--- src/main/java/com/pdetoni/course/services/UserService.java
+++ src/main/java/com/pdetoni/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.pdetoni.course.services;
 
 import com.pdetoni.course.entities.User;
 import com.pdetoni.course.repositories.UserRepository;
+import com.pdetoni.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
